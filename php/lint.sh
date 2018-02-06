05c6780a7c13a2a60e0e77dd23607aa580c0ee4a
Daniel Collins <mail@dcollins.info>
2018-02-07 03:58:33 +1100

    attempt to fix config path

--- php/lint.sh
+++ php/lint.sh
@@ -4,7 +4,7 @@ out=$(find php/ -name \*.php -print0 | xargs -I{} -0 php -l {} | \
       grep -v "No syntax errors detected in ")
 
 phpmd php/ text php/ruleset.xml && \
-phpcs -s -q . && \
+phpcs --standard=php/phpcs.xml -s -q . && \
 if [ -n "$out" ]; then
    echo $out
    exit 1
