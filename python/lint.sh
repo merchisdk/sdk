00248a3e7ff0749365f9351c94e814c4bd8db5a1
Daniel Collins <mail@dcollins.info>
2018-02-07 20:10:54 +1100

    teach travis to lint python

---
+++ python/lint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+PYFILES=`find python/ -type f -name "*.py"
+mypy --ignore-missing-imports --check-untyped-defs --strict-optional\
+      --follow-imports=skip \
+      --warn-no-return --warn-redundant-casts --warn-unused-ignores\
+      $PYFILES || exit 1
+pycodestyle $PYFILES || exit 1
+pydocstyle $PYFILES || exit 1
+flake8 $PYFILES || exit 1
+pylint $PYFILES || exit 1
