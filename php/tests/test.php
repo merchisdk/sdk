07fe14fde57b180c03df17c6c1ddefa210acfdc1
Daniel Collins <mail@dcollins.info>
2018-02-14 22:14:58 +1100

    teach PHP SDK about category entities

--- php/tests/test.php
+++ php/tests/test.php
@@ -5,12 +5,18 @@ use PHPUnit\Framework\TestCase;
 
 require_once 'php/src/products.php';
 require_once 'php/src/util/address.php';
+require_once 'php/src/categories.php';
 
 final class PhpSuite extends TestCase
 {
     public function testCanCreateProduct()
     {
         new Product();
+    }
+
+    public function testCanCreateCategory()
+    {
+        $this->assertNotNull(new Category());
     }
 
     public function testAddressCountry()
