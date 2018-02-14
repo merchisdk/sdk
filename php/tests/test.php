eacf1558c511779c6ad2b85f758a533c18b6281a
Daniel Collins <mail@dcollins.info>
2018-02-14 21:47:06 +1100

    - fix error in constructor of PHP bank and backup entities - add regression tests

--- php/tests/test.php
+++ php/tests/test.php
@@ -4,11 +4,23 @@ declare(strict_types = 1);
 use PHPUnit\Framework\TestCase;
 
 require_once 'php/src/products.php';
+require_once 'php/src/backups.php';
+require_once 'php/src/bank.php';
 
 final class PhpSuite extends TestCase
 {
     public function testCanCreateProduct()
     {
         new Product();
+    }
+
+    public function testCanCreateBackup()
+    {
+        $this->assertNotNull(new Backup());
+    }
+
+    public function testCanCreateBank()
+    {
+        $this->assertNotNull(new Bank());
     }
 }
