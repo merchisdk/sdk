a931d6a02d6845ff6166bcb8154419163663c42e
Daniel Collins <mail@dcollins.info>
2018-02-14 22:32:01 +1100

    teach PHP SDK about CountryTax entities.
    this is a cleaned up version of work by @jack_loves_coding

--- php/tests/test.php
+++ php/tests/test.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 use PHPUnit\Framework\TestCase;
 
+require_once 'php/src/country_taxes.php';
 require_once 'php/src/products.php';
 require_once 'php/src/util/address.php';
 
@@ -16,5 +17,15 @@ final class PhpSuite extends TestCase
     public function testAddressCountry()
     {
         $this->assertEquals(address_country('AU'), 'Australia');
+    }
+
+    public function testCountryTax()
+    {
+        $tax = new CountryTax();
+        $tax->country = 'AU';
+        $tax->tax_name = 'GST';
+
+        $this->assertEquals($tax->country_name(), 'Australia');
+        $this->assertEquals($tax->full_name(), 'GST (Australia)');
     }
 }
