e6a02825ccd798a24238ceafd32dde2c697e7916
Daniel Collins <mail@dcollins.info>
2018-02-15 02:43:17 +1100

    teach PHP SDK about domain invitation entities.
    this is a cleaned up version of work by @jack_loves_coding

--- php/tests/test.php
+++ php/tests/test.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 use PHPUnit\Framework\TestCase;
 
 require_once 'php/src/products.php';
+require_once 'php/src/domain_invitations.php';
 require_once 'php/src/util/address.php';
 
 final class PhpSuite extends TestCase
@@ -11,6 +12,11 @@ final class PhpSuite extends TestCase
     public function testCanCreateProduct()
     {
         new Product();
+    }
+
+    public function testCanCreateDomainInvitation()
+    {
+        $this->assertNotNull(new DomainInvitation());
     }
 
     public function testAddressCountry()
