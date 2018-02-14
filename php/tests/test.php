6ad80794d4eb4d4b40cfbccf7f108cf9442bce8c
Daniel Collins <mail@dcollins.info>
2018-02-15 01:53:21 +1100

    teach PHP SDK about Discount entities.
    this is a cleaned up version of work by @jack_loves_coding.

--- php/tests/test.php
+++ php/tests/test.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 use PHPUnit\Framework\TestCase;
 
 require_once 'php/src/products.php';
+require_once 'php/src/discounts.php';
 require_once 'php/src/util/address.php';
 
 final class PhpSuite extends TestCase
@@ -11,6 +12,11 @@ final class PhpSuite extends TestCase
     public function testCanCreateProduct()
     {
         new Product();
+    }
+
+    public function testCanCreateDiscount()
+    {
+        $this->assertNotNull(new Discount());
     }
 
     public function testAddressCountry()
