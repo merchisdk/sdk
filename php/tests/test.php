17bb1395ca13ea40d7e310535ebb19c5a5921a60
Daniel Collins <mail@dcollins.info>
2018-02-16 17:53:14 +1100

    Teach PHP SDK to be able to format currency amounts using local (#33)
    currency symbols.
    
    This is a cleaned up version of work by @jack_loves_coding.

--- php/tests/test.php
+++ php/tests/test.php
@@ -12,6 +12,7 @@ require_once 'php/src/backups.php';
 require_once 'php/src/bank.php';
 require_once 'php/src/bid_items.php';
 require_once 'php/src/util/address.php';
+require_once 'php/src/util/money_protocol.php';
 require_once 'php/src/categories.php';
 
 final class PhpSuite extends TestCase
@@ -65,6 +66,11 @@ final class PhpSuite extends TestCase
     public function testAddressCountry()
     {
         $this->assertEquals(address_country('AU'), 'Australia');
+    }
+
+    public function testFormatCurrency()
+    {
+        $this->assertEquals(format_currency(200.234, 'AUD', 1), '$200.2');
     }
 
     public function testCountryTax()
