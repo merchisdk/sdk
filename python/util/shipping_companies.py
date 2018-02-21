a29cffc37eae597997179c6c9865a9aa28b668b5
Jim Power <jimpower@Jims-MacBook-Pro.local>
2018-02-21 14:11:43 +1100

    add fastway to list of shipment companies

--- python/util/shipping_companies.py
+++ python/util/shipping_companies.py
@@ -1,4 +1,4 @@
 # when adding new company, make sure last elem in list is custom
 SHIPPING_COMPANIES = ['DHL', 'UPS', 'EMS', 'FedEx',
                       'Australia Post', 'StarTrack', 'Toll', 'TNT',
-                      'Custom Track Link']
+                      'Custom Track Link', 'Fastway']
