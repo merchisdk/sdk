ce53a1e58e7c1ef10bbcea0f21b0ab03ef9b4cc2
Jim Power <jimpower@Jims-MacBook-Pro.local>
2018-02-21 14:26:15 +1100

    add realtime express and uber to shipping companies

--- python/util/shipping_companies.py
+++ python/util/shipping_companies.py
@@ -1,4 +1,5 @@
 # when adding new company, make sure last elem in list is custom
 SHIPPING_COMPANIES = ['DHL', 'UPS', 'EMS', 'FedEx',
                       'Australia Post', 'StarTrack', 'Toll', 'TNT',
-                      'Custom Track Link', 'Fastway']
+                      'Custom Track Link', 'Fastway',
+                      'realtime express', 'Uber']
