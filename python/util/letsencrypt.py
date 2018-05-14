eb671de5ffec55cbf4d6d48580e9d336606a6799
Daniel Collins <mail@dcollins.info>
2018-05-14 19:54:22 +1000

    Use wildcard HTTPS certifcate instead of adding each subdomain individually.

--- python/util/letsencrypt.py
+++ python/util/letsencrypt.py
@@ -1,30 +1,31 @@
-""" Tools to create or renew certificates issues by letsencrypt.
+""" Tools to create or renew certificates issues by letsencrypt. """
 
-    Note that to allow this to be run non-root the script
-    `allow_merchi_user_install_certs.sh` or equivilent should first be run
-    to grant sudo priviledges for the commands used here. Likewise that file
-    should be updated if any of the commands used here change.
-"""
-
+import os
 import subprocess
 import shlex
 import logging
 
 
-def fetch_cert(email, cert_name, domain_names, log):
+def fetch_cert(email, cert_name, domain_names, log, config):
     """ Contact letsencrypt servers and ask them for a new certificate. """
-    cmd = ('/usr/bin/certbot certonly --renew-by-default ' +
+    cmd = ('certbot certonly --renew-by-default ' +
            '--cert-name ' + cert_name + ' ' +
-           '--non-interactive --agree-tos --email {} --server ' +
-           'https://acme-v01.api.letsencrypt.org/directory --webroot -w' +
-           ' /tmp/letsencrypt-auto/ --config-dir /etc/letsencrypt/config ' +
-           '--logs-dir /etc/letsencrypt/logs --work-dir /etc/letsencrypt/work ')
+           '--non-interactive --agree-tos --email {} ' +
+           '--dns-route53 ' +
+           '--server https://acme-v02.api.letsencrypt.org/directory ' +
+           '--config-dir /etc/letsencrypt/config ' +
+           '--logs-dir /etc/letsencrypt/logs ' +
+           '--work-dir /etc/letsencrypt/work')
     cmd = cmd.format(shlex.quote(email))
     for domain_name in domain_names:
-        cmd += ' -d ' + shlex.quote(domain_name)
+        cmd += ' -d' + shlex.quote(domain_name)
     log_msg = "renew cmd: '{}'".format(cmd)
     log(logging.INFO, log_msg)
-    output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    env = os.environ.copy()
+    env["AWS_ACCESS_KEY_ID"] = config['AWS_ACCESS_KEY_ID']
+    env["AWS_SECRET_ACCESS_KEY"] = config['AWS_SECRET_ACCESS_KEY']
+    output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT,
+                                     env=env)
     log_msg = "certbot output: '{}'".format(output)
     log(logging.INFO, log_msg)
 
@@ -48,14 +49,11 @@ def load_cert(log):
     # restart themselves, and ignore any failures, rather than finding out
     # which, if any, are actually running
     restart_container("dockercompose_proxy_1")
-    restart_container("dockercompose_proxy_setup_1")
     restart_container("admin_proxy_1")
-    restart_container("admin_proxy_setup_1")
     restart_container("merchi_proxy_1")
-    restart_container("merchi_proxy_setup_1")
 
 
-def renew_certs(email, cert_name, domain_names, log):
+def renew_certs(email, cert_name, domain_names, log, config):
     """ Create certificate for the given list of str domain names, using the
         provided email address for the letsencrypt account, then load the
         result into NGINX. Invoking for the first time will set up the
@@ -63,5 +61,5 @@ def renew_certs(email, cert_name, domain_names, log):
         the exisiting certificate (an operation which is less heavily rate
         limited by letsencrypt).
     """
-    fetch_cert(email, cert_name, domain_names, log)
+    fetch_cert(email, cert_name, domain_names, log, config)
     load_cert(log)
