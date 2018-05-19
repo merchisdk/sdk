6ba8a3a1e58ab3fabcd8c20aed371b94f481f521
Daniel Collins <mail@dcollins.info>
2018-05-19 17:19:18 +1000

    remove excess whitespace around operators

--- php/src/util/brand_util.php
+++ php/src/util/brand_util.php
@@ -1,6 +1,5 @@
 <?php
 
-const COLOUR_LOGO_URI =
-    'backend/static/img/merchi-master-colour-with-monster.png';
+const COLOUR_LOGO_URI = 'backend/static/img/merchi-master-colour-with-monster.png';
 
 const PLATFORM_MASCOT_ICON = '/static/images/merchi-monster-blue.png';
