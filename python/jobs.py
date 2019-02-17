186ff5d7c97d57c77c20cc19eee78bbe48909c79
Jim Power <jimpower@Jims-MacBook-Pro.local>
2019-02-18 09:46:22 +1100

    remove assignment and assignments from Job entity

--- python/jobs.py
+++ python/jobs.py
@@ -4,7 +4,6 @@ import datetime
 import sdk.python.entities
 from sdk.python.job_comments import JobComment
 from sdk.python.draft_comments import DraftComment
-from sdk.python.production_comments import ProductionComment
 from sdk.python.country_taxes import CountryTax
 from sdk.python.domains import Domain
 from sdk.python.domain_tags import DomainTag
@@ -434,31 +433,4 @@ class Jobs(sdk.python.entities.Resource):
     json_name = 'jobs'
 
 
-class Assignment(sdk.python.entities.Entity):
-
-    resource = '/assignments/'
-    json_name = 'assignment'
-
-    id = Property(int)
-    manager_accepts = Property(str)
-    supplier_refused = Property(str)
-    archived = Property(datetime.datetime)
-    production_deadline = Property(datetime.datetime)
-    assignment_deadline = Property(datetime.datetime)
-    job = Property(Job, backref="assignments")
-    supply_job = Property(Job, backref="supply_assignment")
-    supplier = Property('sdk.python.users.User')
-    bid = Property(Bid, backref="assignments")
-    comments = Property(ProductionComment, backref="assignment")
-    shipment = Property(Shipment, backref="assignments")
-    notifications = Property(Notification)
-
-
-class Assignments(sdk.python.entities.Resource):
-
-    entity_class = Assignment
-    json_name = 'assignments'
-
-
 jobs = Jobs()
-assignments = Assignments()
