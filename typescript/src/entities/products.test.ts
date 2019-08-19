4015b8aed0515d4a64f887034c47dd0652fd3909
Daniel Collins <mail@dcollins.info>
2019-08-20 05:18:53 +1000

    proposed method for teaching typescript SDK about the attributes owned by each type of entity.
    I tried to strike a balance between using advanced/new/experimental features/
    writing complex code, vs the crazy boilerplate if everything is entirely
    manual. That lead me to use decorators to collect the list of
    attributes, but to give up on the more adventurous plan of using
    typescript transfomers to produce the properties themselves.

--- typescript/src/entities/products.test.ts
+++ typescript/src/entities/products.test.ts
@@ -4,3 +4,9 @@ test('can make product', () => {
   const product = new Product();
   expect(product).toBeTruthy();
 });
+
+test('can get and set id', () => {
+  const product = new Product();
+  product.id = 2;
+  expect(product.id).toBe(2);
+});
