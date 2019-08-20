89e82325125bc3390c82dff89617dccda9246c74
Daniel Collins <mail@dcollins.info>
2019-08-20 07:41:43 +0000

    add some more useful error codes (#179)
    * add some more useful error codes
    
    * add missing whitespace before comment

--- python/util/errors.py
+++ python/util/errors.py
@@ -13,3 +13,21 @@ SUPPLIER_NOT_ADDED_TO_PRODUCT = 4
 EMAIL_NOT_FOUND = 5
 INVALID_PHONE_NUMBER = 6
 INVALID_PASSWORD = 7
+RESOURCE_NOT_FOUND = 8  # i.e. HTTP 404
+RESOURCE_GONE = 9  # a requested object has been deleted (i.e. HTTP 410)
+REQUEST_TOO_LARGE = 10  # too many bytes sent or too complex a query
+UNKNOWN_DOMAIN = 11  # request comes from a domain that is not allowed
+UNSUPPORTED_METHOD = 12  # i.e. HTTP 405
+
+# the following are websocket related
+MISSING_EVENT_TYPES = 13
+INVALID_EVENT_TYPES = 14
+MISSING_URI = 15
+MISSING_SID = 16
+MISSING_SECRET_KEY = 17
+MISSING_SUBSCRIPTION_TOKEN = 18
+WRONG_SECRET_KEY = 19
+DUPLICATE_SUBSCRIPTION_TOKEN = 20
+
+BAD_SMS_TOKEN = 21
+DOMAIN_NOT_FOUND = 22  # operation tried with non existing domain
