e860d56b94debfbcaa0680283c6e7e021c2114bf
Daniel Collins <mail@dcollins.info>
2019-09-10 06:29:21 +1000

    proposal for means of reducing number of lines of code needed to define an attribute in typescript SDK.
    internally, this dynamically attaches properties behind the back of the
    type checker. it uses a lot of `any` type. but, externally, the attributes
    are still properly typed.

--- typescript/src/entities/product.ts
+++ typescript/src/entities/product.ts
@@ -8,54 +8,14 @@ export class Product extends Entity {
   protected static pluralName: string = "products";
 
   @Product.property("id")
-  private _id?: number;
-
-  get id(): number | undefined {
-    return this._id;
-  }
-
-  set id(newId: number | undefined) {
-    this._id = newId;
-    this.markDirty("id", newId);
-  }
+  public id?: number;
 
   @Product.property("name")
-  private _name?: string;
-
-  get name(): string | undefined {
-    return this._name;
-  }
-
-  set name(newName: string | undefined) {
-    this._name = newName;
-    this.markDirty("name", newName);
-  }
+  public name?: string;
 
   @Product.property("categories", "Category")
-  private _categories?: Array<Category>;
-
-  get categories(): Array<Category> | undefined {
-    return this._categories;
-  }
-
-  set categories(newCategories: Array<Category> | undefined) {
-    this._categories = newCategories;
-    this.checkSameSessionList(newCategories);
-    this.addBackObjectList(newCategories);
-    this.markDirty("categories", newCategories)
-  }
+  public categories?: Array<Category>;
 
   @Product.property("domain")
-  private _domain?: Domain;
-
-  get domain(): Domain | undefined {
-    return this._domain;
-  }
-
-  set domain(newDomain: Domain | undefined) {
-    this.checkSameSession(newDomain);
-    this._domain = newDomain;
-    this.addBackObject(newDomain);
-    this.markDirty("domain", newDomain)
-  }
+  public domain?: Domain;
 }
