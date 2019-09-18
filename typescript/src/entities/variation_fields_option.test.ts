4129ca6bf64cf587f3dac96ba488e8c07885d0e0
Daniel Collins <mail@dcollins.info>
2019-09-19 05:26:51 +1000

    ts sdk enttiy VariationFieldsOption has method `totalCost`

--- typescript/src/entities/variation_fields_option.test.ts
+++ typescript/src/entities/variation_fields_option.test.ts
@@ -5,3 +5,13 @@ test('can make VariationFieldsOption', () => {
   const variationFieldsOption = new merchi.VariationFieldsOption();
   expect(variationFieldsOption).toBeTruthy();
 });
+
+test('totalCost', () => {
+  const merchi = new Merchi();
+  const vfo = new merchi.VariationFieldsOption();
+  expect(vfo.totalCost).toThrow();
+  vfo.variationCost = 12.4;
+  expect(vfo.totalCost).toThrow();
+  vfo.variationUnitCost = 9.98;
+  expect(vfo.totalCost(33)).toEqual(341.74);
+});
