f7fcd81c199700fdd28374cbc0ebfe3fcbb783d9
Jim Power <jimpower@Jims-MacBook-Pro.local>
2019-09-21 11:15:17 +1000

    add shipment methods and shipment method variations to sdk

---
+++ python/shipment_methods.py
@@ -0,0 +1,29 @@
+import sdk.python.entities
+from sdk.python.addresses import Address
+from sdk.python.entities import Property
+from sdk.python.shipment_method_variations import ShipmentMethodVariation
+
+
+class ShipmentMethod(sdk.python.entities.Entity):
+
+    resource = '/shipment_methods/'
+    json_name = 'shipmentMethod'
+
+    id = Property(int)
+    name = Property(str)
+    originAddress = Property(Address)
+    company = Property("sdk.python.companies.Company")
+    default_cost = Property(float)
+    currency = Property(str)
+    transport_company = Property(str)
+    tax_type = Property(sdk.python.country_taxes.CountryTax)
+    variations = Property(ShipmentMethodVariation)
+
+
+class ShipmentMethods(sdk.python.entities.Resource):
+
+    entity_class = ShipmentMethod
+    json_name = 'shipmentMethods'
+
+
+shipment_methods = ShipmentMethods()
