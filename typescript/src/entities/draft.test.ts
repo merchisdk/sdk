134a8defc465110b66b9239fba7b136d9b25d285
Daniel Collins <mail@dcollins.info>
2019-09-23 19:23:27 +1000

    teach ts sdk about draft utility methods

--- typescript/src/entities/draft.test.ts
+++ typescript/src/entities/draft.test.ts
@@ -5,3 +5,30 @@ test('can make Draft', () => {
   const draft = new merchi.Draft();
   expect(draft).toBeTruthy();
 });
+
+test('whereChangesRequested', () => {
+  const merchi = new Merchi();
+  const draft = new merchi.Draft();
+  expect(draft.whereChangesRequested).toThrow();
+  draft.comments = [];
+  expect(draft.whereChangesRequested()).toBe(false);
+  draft.comments = [new merchi.DraftComment()]; 
+  expect(draft.whereChangesRequested).toThrow();
+  draft.comments[0].changeRequest = false;
+  expect(draft.whereChangesRequested()).toBe(false);
+  draft.comments[0].changeRequest = true;
+  expect(draft.whereChangesRequested()).toBe(true);
+});
+
+test('commentsYoungestToEldest', () => {
+  const merchi = new Merchi();
+  const draft = new merchi.Draft();
+  expect(draft.commentsYoungestToEldest).toThrow();
+  draft.comments = [];
+  expect(draft.commentsYoungestToEldest()).toEqual([]);
+  draft.comments = [new merchi.DraftComment(), new merchi.DraftComment()];
+  expect(draft.commentsYoungestToEldest).toThrow();
+  draft.comments[0].id = 2;
+  draft.comments[1].id = 1;
+  expect(draft.commentsYoungestToEldest()[0].id).toEqual(1);
+});
