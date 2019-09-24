5fd00be137d794f2055457a2e3e1d9457fc204b2
Daniel Collins <mail@dcollins.info>
2019-09-24 14:18:35 +0700

    teach ts sdk about cart entity util methods (#231)

--- typescript/src/entities/cart.test.ts
+++ typescript/src/entities/cart.test.ts
@@ -5,3 +5,17 @@ test('can make Cart', () => {
   const cart = new merchi.Cart();
   expect(cart).toBeTruthy();
 });
+
+test('requiresShipment', () => {
+  const merchi = new Merchi();
+  const cart = new merchi.Cart();
+  expect(cart.requiresShipment).toThrow();
+  cart.cartItems = [];
+  expect(cart.requiresShipment()).toBe(false);
+  cart.cartItems = [new merchi.CartItem()];
+  cart.cartItems[0].product = new merchi.Product();
+  cart.cartItems[0].product.needsShipping = true;
+  expect(cart.requiresShipment()).toBe(true);
+  cart.cartItems[0].product.needsShipping = false;
+  expect(cart.requiresShipment()).toBe(false);
+});
