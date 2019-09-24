2a92152a65620e8ed9e3dab1911c649ae7b4ba81
Amtron <james@newgenstudio.com>
2019-09-24 10:53:41 +1000

    extend shipments (#234)

--- python/shipments.py
+++ python/shipments.py
@@ -6,6 +6,7 @@ from sdk.python.domain_tags import DomainTag
 from sdk.python.users import User
 from sdk.python.companies import Company
 from sdk.python.invoices import Invoice
+from sdk.python.shipment_methods import ShipmentMethod
 
 
 class Shipment(sdk.python.entities.Entity):
@@ -51,6 +52,7 @@ class Shipment(sdk.python.entities.Entity):
     tax_amount = Property(float)
     tax_type = Property(sdk.python.country_taxes.CountryTax)
     tags = Property(DomainTag, backref="shipments")
+    shipment_method = Property(ShipmentMethod)
 
 
 class Shipments(sdk.python.entities.Resource):
