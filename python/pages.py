9f340bd67df061a4c7f9bce21e85864afbf54b34
Amtron <james@newgenstudio.com>
2019-10-21 10:24:20 +1100

    Add theme pages (#245)
    * add pages to themes
    
    * add template to pages

---
+++ python/pages.py
@@ -0,0 +1,13 @@
+import sdk.python.entities
+from sdk.python.entities import Property
+
+
+class Page(sdk.python.entities.Entity):
+
+    resource = '/pages/'
+    json_name = 'page'
+
+    id = Property(int)
+    name = Property(str)
+    slug = Property(str)
+    template = Property(str)
