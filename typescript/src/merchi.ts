51c62c7f08bedc5ca0601f90d61f175a0ec4e34b
chengyu <chengyu0316@gmail.com>
2019-11-07 11:33:01 +1100

    add get current user function and add remove extra trailing white space

--- typescript/src/merchi.ts
+++ typescript/src/merchi.ts
@@ -1,66 +1,69 @@
-import { Entity } from './entity';
-import { Session } from './entities/session';
-import { JobComment } from './entities/job_comment';
-import { Domain } from './entities/domain';
-import { Job } from './entities/job';
-import { Menu } from './entities/menu';
-import { Backup } from './entities/backup';
-import { VariationField } from './entities/variation_field';
-import { ProductionComment } from './entities/production_comment';
-import { Product } from './entities/product';
-import { Inventory } from './entities/inventory';
-import { BidItem } from './entities/bid_item';
-import { Category } from './entities/category';
-import { Invoice } from './entities/invoice';
-import { UserCompany } from './entities/user_company';
-import { InventoryUnitVariation } from './entities/inventory_unit_variation';
-import { VariationFieldsOption } from './entities/variation_fields_option';
-import { Bank } from './entities/bank';
-import { Shipment } from './entities/shipment';
-import { DomainInvitation } from './entities/domain_invitation';
-import { EmailCounter } from './entities/email_counter';
-import { MenuItem } from './entities/menu_item';
-import { SupplyDomain } from './entities/supply_domain';
-import { Cart } from './entities/cart';
-import { Theme } from './entities/theme';
-import { Component } from './entities/component';
-import { MerchiFile } from './entities/file';
-import { EmailAddress } from './entities/email_address';
-import { ShortUrl } from './entities/short_url';
-import { VariationsGroup } from './entities/variations_group';
-import { Bid } from './entities/bid';
-import { Draft } from './entities/draft';
-import { Discount } from './entities/discount';
-import { User } from './entities/user';
-import { Company } from './entities/company';
-import { ComponentTag } from './entities/component_tag';
-import { EnrolledDomain } from './entities/enrolled_domain';
-import { CountryTax } from './entities/country_tax';
-import { Item } from './entities/item';
-import { DomainTag } from './entities/domain_tag';
-import { DraftComment } from './entities/draft_comment';
-import { Notification } from './entities/notification';
-import { Payment } from './entities/payment';
-import { CompanyInvitation } from './entities/company_invitation';
-import { SystemRole } from './entities/system_role';
-import { PhoneNumber } from './entities/phone_number';
-import { Variation } from './entities/variation';
-import { CartItem } from './entities/cart_item';
-import { Address } from './entities/address';
-import { Assignment } from './entities/assignment';
-import { generateUUID } from './uuid';
+import { Entity } from "./entity";
+import { Session } from "./entities/session";
+import { JobComment } from "./entities/job_comment";
+import { Domain } from "./entities/domain";
+import { Job } from "./entities/job";
+import { Menu } from "./entities/menu";
+import { Backup } from "./entities/backup";
+import { VariationField } from "./entities/variation_field";
+import { ProductionComment } from "./entities/production_comment";
+import { Product } from "./entities/product";
+import { Inventory } from "./entities/inventory";
+import { BidItem } from "./entities/bid_item";
+import { Category } from "./entities/category";
+import { Invoice } from "./entities/invoice";
+import { UserCompany } from "./entities/user_company";
+import { InventoryUnitVariation } from "./entities/inventory_unit_variation";
+import { VariationFieldsOption } from "./entities/variation_fields_option";
+import { Bank } from "./entities/bank";
+import { Shipment } from "./entities/shipment";
+import { DomainInvitation } from "./entities/domain_invitation";
+import { EmailCounter } from "./entities/email_counter";
+import { MenuItem } from "./entities/menu_item";
+import { SupplyDomain } from "./entities/supply_domain";
+import { Cart } from "./entities/cart";
+import { Theme } from "./entities/theme";
+import { Component } from "./entities/component";
+import { MerchiFile } from "./entities/file";
+import { EmailAddress } from "./entities/email_address";
+import { ShortUrl } from "./entities/short_url";
+import { VariationsGroup } from "./entities/variations_group";
+import { Bid } from "./entities/bid";
+import { Draft } from "./entities/draft";
+import { Discount } from "./entities/discount";
+import { User } from "./entities/user";
+import { Company } from "./entities/company";
+import { ComponentTag } from "./entities/component_tag";
+import { EnrolledDomain } from "./entities/enrolled_domain";
+import { CountryTax } from "./entities/country_tax";
+import { Item } from "./entities/item";
+import { DomainTag } from "./entities/domain_tag";
+import { DraftComment } from "./entities/draft_comment";
+import { Notification } from "./entities/notification";
+import { Payment } from "./entities/payment";
+import { CompanyInvitation } from "./entities/company_invitation";
+import { SystemRole } from "./entities/system_role";
+import { PhoneNumber } from "./entities/phone_number";
+import { Variation } from "./entities/variation";
+import { CartItem } from "./entities/cart_item";
+import { Address } from "./entities/address";
+import { Assignment } from "./entities/assignment";
+import { generateUUID } from "./uuid";
 // eslint-disable-next-line no-unused-vars
-import { RequestOptions, apiFetch } from './request';
-import { getCookie } from './cookie';
-
+import { RequestOptions, apiFetch } from "./request";
+import { getCookie } from "./cookie";
 
 // the type of classes
-export interface Type<T, A extends any[]> extends
-   Function { new(...args: A): T; }
+export interface Type<T, A extends any[]> extends Function {
+  new (...args: A): T;
+}
 
-function cloneClass<T, A extends []>(original: Type<T, A>, arg: any): Type<T, A> {
+function cloneClass<T, A extends []>(
+  original: Type<T, A>,
+  arg: any
+): Type<T, A> {
   // copy the constructor, but use the empty object as `this`
-  const copy = original.bind({} , arg);
+  const copy = original.bind({}, arg);
   // pick up any static members (this is shallow, the members are not copied)
   Object.assign(copy, original);
   return copy;
@@ -125,7 +128,7 @@ export class Merchi {
     if (sessionToken) {
       this.sessionToken = sessionToken;
     } else {
-      this.sessionToken = getCookie('session_token');
+      this.sessionToken = getCookie("session_token");
     }
     function setupClass(merchi: Merchi, cls: typeof Entity) {
       // copy, to prevent interference from other merchi sessions
@@ -148,19 +151,37 @@ export class Merchi {
     this.Job = setupClass(this, Job) as typeof Job;
     this.ComponentTag = setupClass(this, ComponentTag) as typeof ComponentTag;
     this.Category = setupClass(this, Category) as typeof Category;
-    this.VariationField = setupClass(this, VariationField) as typeof VariationField;
-    this.InventoryUnitVariation = setupClass(this, InventoryUnitVariation) as typeof InventoryUnitVariation;
+    this.VariationField = setupClass(
+      this,
+      VariationField
+    ) as typeof VariationField;
+    this.InventoryUnitVariation = setupClass(
+      this,
+      InventoryUnitVariation
+    ) as typeof InventoryUnitVariation;
     this.PhoneNumber = setupClass(this, PhoneNumber) as typeof PhoneNumber;
     this.BidItem = setupClass(this, BidItem) as typeof BidItem;
     this.Menu = setupClass(this, Menu) as typeof Menu;
     this.Assignment = setupClass(this, Assignment) as typeof Assignment;
     this.Draft = setupClass(this, Draft) as typeof Draft;
-    this.VariationsGroup = setupClass(this, VariationsGroup) as typeof VariationsGroup;
-    this.EnrolledDomain = setupClass(this, EnrolledDomain) as typeof EnrolledDomain;
-    this.CompanyInvitation = setupClass(this, CompanyInvitation) as typeof CompanyInvitation;
+    this.VariationsGroup = setupClass(
+      this,
+      VariationsGroup
+    ) as typeof VariationsGroup;
+    this.EnrolledDomain = setupClass(
+      this,
+      EnrolledDomain
+    ) as typeof EnrolledDomain;
+    this.CompanyInvitation = setupClass(
+      this,
+      CompanyInvitation
+    ) as typeof CompanyInvitation;
     this.Bid = setupClass(this, Bid) as typeof Bid;
     this.EmailAddress = setupClass(this, EmailAddress) as typeof EmailAddress;
-    this.ProductionComment = setupClass(this, ProductionComment) as typeof ProductionComment;
+    this.ProductionComment = setupClass(
+      this,
+      ProductionComment
+    ) as typeof ProductionComment;
     this.Backup = setupClass(this, Backup) as typeof Backup;
     this.CountryTax = setupClass(this, CountryTax) as typeof CountryTax;
     this.ShortUrl = setupClass(this, ShortUrl) as typeof ShortUrl;
@@ -169,11 +190,17 @@ export class Merchi {
     this.CartItem = setupClass(this, CartItem) as typeof CartItem;
     this.UserCompany = setupClass(this, UserCompany) as typeof UserCompany;
     this.DomainTag = setupClass(this, DomainTag) as typeof DomainTag;
-    this.VariationFieldsOption = setupClass(this, VariationFieldsOption) as typeof VariationFieldsOption;
+    this.VariationFieldsOption = setupClass(
+      this,
+      VariationFieldsOption
+    ) as typeof VariationFieldsOption;
     this.Address = setupClass(this, Address) as typeof Address;
     this.Item = setupClass(this, Item) as typeof Item;
     this.SupplyDomain = setupClass(this, SupplyDomain) as typeof SupplyDomain;
-    this.DomainInvitation = setupClass(this, DomainInvitation) as typeof DomainInvitation;
+    this.DomainInvitation = setupClass(
+      this,
+      DomainInvitation
+    ) as typeof DomainInvitation;
     this.EmailCounter = setupClass(this, EmailCounter) as typeof EmailCounter;
     this.Session = setupClass(this, Session) as typeof Session;
     this.Bank = setupClass(this, Bank) as typeof Bank;
@@ -192,8 +219,17 @@ export class Merchi {
         /* istanbul ignore next */
         options.query = [];
       }
-      options.query.push(['session_token', this.sessionToken]);
+      options.query.push(["session_token", this.sessionToken]);
     }
     return apiFetch(resource, options);
-  }
+  };
+
+  public getCurrentUser = () => {
+    if (!this.sessionToken) {
+      return Promise.resolve(null);
+    }
+    return this.Session.get(this.sessionToken, {
+      embed: { user: { enrolledDomains: {} } }
+    }).then((session: any) => session.user);
+  };
 }
