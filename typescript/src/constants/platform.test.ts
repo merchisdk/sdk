acb6dc33014dea55e672847b3246494b0490a603
chengyu <chengyu0316@gmail.com>
2019-11-18 19:34:39 +1100

    add platform related constants to sdk

---
+++ typescript/src/constants/platform.test.ts
@@ -0,0 +1,13 @@
+import {
+  platformName,
+  platformCopyright,
+  platfromSellerDomain,
+  platfromSellerDomainPlus
+} from "./platform";
+
+test("platform variables all there", () => {
+  expect(platformName).toBe('merchi');
+  expect(platformCopyright).toBe(2019);
+  expect(platfromSellerDomain).toBe('merchi.me');
+  expect(platfromSellerDomainPlus).toBe('merchi.store');
+});
