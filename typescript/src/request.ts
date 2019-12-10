53c283fa51e1a67e8d2a3554742caa0657583587
Cheng Yu <chengyu0316@gmail.com>
2019-12-10 17:28:43 +1100

    error.msg enqual to err.message string rather than jsonify result (#270)
    * error.msg enqual to err.message string rather than jsonify result
    
    * remove extra }

--- typescript/src/request.ts
+++ typescript/src/request.ts
@@ -19,7 +19,7 @@ export class ApiError extends Error {
     this.statusCode = err.statusCode;
     this.errorCode = getErrorFromCode(err.errorCode);
     this.name = 'ApiError';
-    this.message = message;
+    this.message = err.message;
   }
 }
 
