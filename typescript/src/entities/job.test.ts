acde0e2425135889f96431683578866ff528a5b0
chengyu <chengyu0316@gmail.com>
2019-12-19 17:38:59 +1100

    lock timezone avoid mis understanding between ci and my local machine

--- typescript/src/entities/job.test.ts
+++ typescript/src/entities/job.test.ts
@@ -9,8 +9,8 @@ test('can make Job', () => {
 test('Job deadline serialised to mini seconds', () => {
   const merchi = new Merchi();
   const job = new merchi.Job();
-  job.deadline = new Date('2012.08.10');
-  const correct = [['deadline', '1344520800']];
+  job.deadline = new Date('Feb 28 2013 19:00:00 GMT-0500');
+  const correct = [['deadline', '1362096000']];
   const backData = Array.from((job.toFormData() as any).entries());
   expect(backData).toEqual(correct);
 });
