fbfaa2962797f1988cf83201e9e200506ebd79de
chengyu <chengyu0316@gmail.com>
2020-03-29 11:06:37 +1100

    port notifications check update function

--- typescript/src/toasts.ts
+++ typescript/src/toasts.ts
@@ -1,55 +1,37 @@
+// eslint-disable-next-line no-unused-vars
+import { RequestOptions } from './request';
+// eslint-disable-next-line no-unused-vars
+import { Merchi } from './merchi';
+// eslint-disable-next-line no-unused-vars
+import { Notification } from './entities/notification';
+// eslint-disable-next-line no-unused-vars
+import { NotificationSection } from './constants/notification_sections';
+// eslint-disable-next-line no-unused-vars
+import { NotificationType } from './constants/notification_types';
 
-export function backendFetch(resource: string, options?: RequestOptions) {
-  const server = (window as any).merchiBackendUri ?
-    (window as any).merchiBackendUri : BACKEND_URI;
-  const version = 'v6';
-  const url = new URL(server + version + resource);
-  if (options && options.query) {
-    for (let entry of options.query) {
-      url.searchParams.append(entry[0], entry[1]);
-    }
-  }
-  return fetch(url.toString(), options);
+interface RelatedObj {
+  id: number,
+  type: string
 }
-    function checkAndUpdateNotifications(related_object_id, section,
-        senderId, success, error) {
-        var request = new Request(),
-            data = new Dictionary();
-        request.resource('/notifications-toast/');
-        request.method('POST');
-        data.add('related_object_id', related_object_id);
-        data.add('section', section);
-        data.add('sender_id', senderId);
-        request.query().add('skip_rights', 'y');
-        request.data().merge(data);
-        function handleResponse(status, body) {
-            var result = '';
-            if (status === 200) {
-                try {
-                    result = JSON.parse(body);
-                } catch (e) {
-                    result = {message: 'invalid json from server',
-                              errorCode: 0};
-                }
-                success(JSON.parse(body));
-            } else {
-                try {
-                    result = JSON.parse(body);
-                } catch (e) {
-                    result = {message: 'could not recover the entity',
-                              errorCode: 0};
-                }
-                error(result);
-            }
-        }
-        function handleError() {
-            error({message: 'could not connect to server',
-                   errorCode: 0});
-        }
-        request.responseHandler(handleResponse).errorHandler(handleError);
-        request.send();
 
-        subscriptionManager.subscribe([eventTypes.get('POST')], request.path(),
-                                      "POST", handleResponse);
-    }
+interface ToastOptions {
+  entities: Array<RelatedObj>;
+  sections: Array<NotificationSection>;
+  notificationTypes: Record<NotificationType, boolean>;
+}
 
+
+export const toastNotifications = (merchi: Merchi, options: ToastOptions) => {
+  const resource = '/notifications-check-update/';
+  const data = new FormData();
+  const { entities, sections, notificationTypes } = options;
+  data.set('entities', JSON.stringify(entities));
+  data.set('sections', JSON.stringify(sections));
+  data.set('notification_types', JSON.stringify(notificationTypes));
+  const fetchOptions: RequestOptions = { method: 'POST', body: data };
+  return merchi
+    .authenticatedFetch(resource, fetchOptions)
+    .then((data: any) =>
+      data.notifications.map((n: any) => new merchi.Notification().fromJson(n))
+    );
+};
