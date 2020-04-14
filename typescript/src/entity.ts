719ea8536deda611709881d682745a3b40e0838f
Jim Power <jimpower@Jims-MacBook-Pro.local>
2020-04-14 14:43:46 +1000

    if quantity or unitPrice are undefined or null assign 0 to them

--- typescript/src/entity.ts
+++ typescript/src/entity.ts
@@ -80,6 +80,7 @@ interface ListOptions {
   businessDomainsOnly?: boolean;
   dateFrom?: Date;
   dateTo?: Date;
+  relatedAssignment?: number;
   relatedJob?: number;
   relatedProduct?: number;
   jobNotifiable?: number;
@@ -418,6 +419,10 @@ export class Entity {
       }
       if (options.dateTo !== undefined) {
         fetchOptions.query.push(['date_to', toUnixTimestamp(options.dateTo)]);
+      }
+      if (options.relatedAssignment !== undefined) {
+        fetchOptions.query.push(['related_assignment',
+                                 options.relatedAssignment.toString()]);
       }
       if (options.relatedJob !== undefined) {
         fetchOptions.query.push(['related_job', options.relatedJob.toString()]);
