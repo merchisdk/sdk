1921634fde0525c626f3e0c857c4f7dc80631012
chengyu <chengyu0316@gmail.com>
2020-05-08 13:25:33 +1000

    add error types

--- python/util/errors.py
+++ python/util/errors.py
@@ -145,3 +145,5 @@ BAD_SLUG = 112  # provided value that cannot be used in url
 NO_DOMAIN_MANAGER = 113  # domain does not have either admin or manager
 
 MISSING_DOMAIN_FOR_SELL = 114  # missing sell domain for supply product
+WRONG_DOMAIN_FOR_SELL = 114  # target for supply product does not exists
+NO_PERMISSION_TO_SELL = 114  # do not have permission to sell target product
