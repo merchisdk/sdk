fce56ff87380274df4f0097ba4e6977ca26bade6
chengyu <chengyu0316@gmail.com>
2020-07-13 10:08:05 +1000

    use envparse so we can do things with type

--- python/util/config.py
+++ python/util/config.py
@@ -1,12 +1,9 @@
-import os
+from envparse import env
 
 PLATFORM_NAME = 'merchi'
 PLATFORM_COPYRIGHT = 2019
 
 
 def load_configuration(app, config_keys):
-    for name in config_keys:
-        value = os.environ.get(name)
-        if value is None:
-            raise RuntimeError("missing config for {}".format(name))
-        app.config[name] = value
+    for name, cast_type in config_keys:
+        app.config[name] = env(name, cast=cast_type)
