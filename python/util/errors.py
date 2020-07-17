c0a28436dcbafe3882dfe459fa94b1b5a81982dd
Cheng Yu <chengyu0316@gmail.com>
2020-07-17 11:57:39 +1000

    add errors for variation related issues (#333)

--- python/util/errors.py
+++ python/util/errors.py
@@ -149,3 +149,6 @@ CONFLICT_PARAMETERS = 114  # query parameters are conflicting with each other
 MISSING_DOMAIN_FOR_SELL = 115  # missing sell domain for supply product
 WRONG_DOMAIN_FOR_SELL = 116  # target for supply product does not exists
 NO_PERMISSION_TO_SELL = 117  # do not have permission to sell target product
+
+DUPLICATE_VARIATION_FIELDS = 118  # product should not have duplicated field
+NO_MATCHED_VARIATION_FIELDS = 119  # product should not have duplicated field
