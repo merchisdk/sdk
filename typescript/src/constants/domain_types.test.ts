e9a24ab632c1008e601ac3e5f83672bc6114772d
Amtron <james@newgenstudio.com>
2020-10-05 20:19:38 +1100

    add domain types to sdk (#354)
    Co-authored-by: Jim Power <jimpower@Jims-MacBook-Pro.local>

---
+++ typescript/src/constants/domain_types.test.ts
@@ -0,0 +1,5 @@
+import { DomainType } from './domain_types';
+
+test('seller type exists', () => {
+  expect(DomainType.SELLER).toBe(1);
+});
---
+++ typescript/src/constants/domain_types.ts
@@ -0,0 +1,7 @@
+export enum DomainType {
+  UNRESTRICTED = 0,
+  SELLER = 1,
+  SELLER_PLUS = 2,
+  SUPPLIER = 3,
+  RESTRICTED_SUPPLIER = 4,
+}
