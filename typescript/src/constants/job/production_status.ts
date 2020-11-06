99b8e2b6a78f8d49d15150d3e643859fcaaa3278
Jim Power <jimpower@Jims-MacBook-Pro.local>
2020-11-06 13:03:50 +1100

    change name of BIDDING constants

--- typescript/src/constants/job/production_status.ts
+++ typescript/src/constants/job/production_status.ts
@@ -1,7 +1,7 @@
 export enum ProductionStatus {
   INIT = 0,
   REJECTED = 1,
-  BIDDING = 2,
+  QUOTING = 2,
   ASSIGN_SENT = 3,
   ASSIGN_DEADLINE_REACHED = 5,
   QUESTIONING = 6,
--- typescript/src/constants/job_status.ts
+++ typescript/src/constants/job_status.ts
@@ -17,7 +17,7 @@ export enum PaymentStatus {
 export enum ProductionStatus {
   INIT = 0,
   REJECTED = 1,
-  BIDDING = 2,
+  QUOTING = 2,
   ASSIGN_SENT = 3,
   ASSIGN_DEADLINE_REACHED = 4,
   ASSIGN_COMPLETE = 5,
