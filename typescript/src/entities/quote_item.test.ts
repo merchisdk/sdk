67482874f21742b132304792ea7a6cad5779b610
Jim Power <jimpower@Jims-MacBook-Pro.local>
2020-11-09 09:46:43 +1100

    add taxtype to quote

--- typescript/src/entities/quote_item.test.ts
+++ typescript/src/entities/quote_item.test.ts
@@ -3,7 +3,7 @@ import { Merchi } from '../merchi';
 test('can make QuoteItem', () => {
   const merchi = new Merchi();
   const quoteItem = new merchi.QuoteItem();
-  expect quoteItem).toBeTruthy();
+  expect(quoteItem).toBeTruthy();
 });
 
 test('total', () => {
@@ -11,9 +11,9 @@ test('total', () => {
   const quoteItem = new merchi.QuoteItem();
   quoteItem.quantity = 3.55;
   quoteItem.unitPrice = null;
-  expect quoteItem.total()).toEqual('0.000');
+  expect(quoteItem.total()).toEqual('0.000');
   quoteItem.unitPrice = 87.12;
-  expect quoteItem.total()).toEqual('309.276');
+  expect(quoteItem.total()).toEqual('309.276');
   quoteItem.quantity = undefined;
-  expect quoteItem.total()).toEqual('0.000');
+  expect(quoteItem.total()).toEqual('0.000');
 });
