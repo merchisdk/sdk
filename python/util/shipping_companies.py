bf5a65e9f6b488bc73a18e3748fb90533407c1b5
Amtron <james@newgenstudio.com>
2020-12-14 08:30:26 +1100

    add new shipment company and change name of Fastway (#380)
    Co-authored-by: Jim Power <jimpower@Jims-MacBook-Pro.local>

--- python/util/shipping_companies.py
+++ python/util/shipping_companies.py
@@ -1,5 +1,6 @@
 # when adding new company, make sure last elem in list is custom
 SHIPPING_COMPANIES = ['DHL', 'UPS', 'EMS', 'FedEx',
                       'Australia Post', 'StarTrack', 'Toll', 'TNT',
-                      'Custom Track Link', 'Fastway',
-                      'realtime express', 'Uber']
+                      'Custom Track Link', 'Aramex',
+                      'realtime express', 'Uber',
+                      'Civic Transport Couriers']
