6855df685f6ca7a40ae84833f95ff111f5ee4782
Daniel Collins <mail@dcollins.info>
2021-01-08 17:26:10 +1100

    Taxes are now per company.

--- python/companies.py
+++ python/companies.py
@@ -24,6 +24,7 @@ class Company(sdk.python.entities.Entity):
     tax_number = Property(str)
     tax_number_type = Property(int)
     default_tax_type = Property(CountryTax)
+    country_taxes = Property(CountryTax, backref="company")
     default_currency = Property(str)
     paypal_account = Property(str)
     paypal_password = Property(str)
--- typescript/src/entities/company.ts
+++ typescript/src/entities/company.ts
@@ -110,6 +110,9 @@ export class Company extends Entity {
   @Company.property({type: CountryTax})
   public defaultTaxType?: CountryTax | null;
 
+  @Company.property({arrayType: 'CountryTax'})
+  public countryTaxes?: CountryTax[];
+
   @Company.property({type: SubscriptionPlan})
   public subscriptionPlan?: SubscriptionPlan | null;
 
