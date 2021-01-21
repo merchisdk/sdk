bcc00885d8ec27b8da5367fe850fb61b19fc85ef
Daniel Collins <mail@dcollins.info>
2021-01-21 18:18:25 +1100

    small code formatting conventions fix

--- typescript/src/util/query_string.ts
+++ typescript/src/util/query_string.ts
@@ -1,8 +1,8 @@
 export function getQueryStringValue(name: string) {
   const query = window.location.search.substring(1);
-  const vars = query.split("&");
+  const vars = query.split('&');
   for (let i = 0; i < vars.length; i++) {
-    let pair = vars[i].split("=");
+    const pair = vars[i].split('=');
     if (pair[0] === name) {
       return decodeURIComponent(pair[1]);
     }
