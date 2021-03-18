34617c543079314e7304b878310e358b35aeb952
chengyu <chengyu0316@gmail.com>
2021-03-18 11:17:41 +1100

    fix testcase failure

--- typescript/src/entities/product.ts
+++ typescript/src/entities/product.ts
@@ -133,7 +133,7 @@ export class Product extends Entity {
   @Product.property({type: MerchiFile})
   public featureImage?: MerchiFile | null;
 
-  @Product.property({type: Job})
+  @Product.property({type: 'Job'})
   public createdByJob?: Job | null;
 
   @Product.property({arrayType: 'Company', jsonName: 'saved_by_companies'})
