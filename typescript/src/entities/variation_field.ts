c0b067129c234cbef18807bb0485de8933258977
chengyu <chengyu0316@gmail.com>
2021-03-26 18:36:07 +1100

    chained relationship should not need to show on field and option

--- typescript/src/entities/variation_field.ts
+++ typescript/src/entities/variation_field.ts
@@ -94,12 +94,6 @@ export class VariationField extends Entity {
   @VariationField.property({arrayType: 'VariationFieldsOption'})
   public options?: VariationFieldsOption[];
 
-  @VariationField.property({type: 'VariationField'})
-  public chainedSupplierProductFieldOption?: VariationField | null;
-
-  @VariationField.property({type: 'VariationField'})
-  public chainedSellerProductFieldOption?: VariationField | null;
-
   public isSelectable = () => {
     if (this.fieldType === undefined) {
       throw new Error('fieldType is undefined, did you forget to embed it?');
