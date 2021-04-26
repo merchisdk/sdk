c5d4505c801dab036add27404ed61a7dbc922117
Jim Power <jimpower@Jims-MacBook-Pro.local>
2021-04-27 07:34:16 +1000

    update platform variables

--- typescript/src/constants/platform.ts
+++ typescript/src/constants/platform.ts
@@ -1,5 +1,6 @@
 export const platformName = 'merchi';
-export const platformCopyright = 2020;
+export const platformCopyright = 2021;
+export const platformDomain = 'merchi.co';
 export const platfromSellerDomain = 'merchi.me';
 export const platfromSellerDomainPlus = 'merchi.store';
 
