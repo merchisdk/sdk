6ceb6df8c0c0b93d3158fba7a14d42a68c0dce39
Jim Power <jimpower@Jims-MacBook-Pro.local>
2021-06-05 12:20:27 +1000

    add products to shipment method

--- python/shipment_methods.py
+++ python/shipment_methods.py
@@ -1,5 +1,6 @@
 import sdk.python.entities
 from sdk.python.addresses import Address
+from sdk.python.products import Product
 from sdk.python.entities import Property
 from sdk.python.shipment_method_variations import ShipmentMethodVariation
 
@@ -16,6 +17,7 @@ class ShipmentMethod(sdk.python.entities.Entity):
     default_cost = Property(float)
     company_default = Property(bool)
     currency = Property(str)
+    products = Property(Product)
     transport_company = Property(str)
     tax_type = Property('sdk.python.country_taxes.CountryTax')
     variations = Property(ShipmentMethodVariation)
