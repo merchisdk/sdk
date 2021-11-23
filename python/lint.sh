bf4571a62cdc2f2a9a0a87199db2006b6927d8e5
Daniel Collins <mail@dcollins.info>
2021-11-24 04:44:27 +1100

    m

--- python/lint.sh
+++ python/lint.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-PYFILES=`find . -type f -name "*.py"`
-echo $PYFILES
+PYFILES=`find python/ -type f -name "*.py"`
 mypy --ignore-missing-imports --check-untyped-defs --strict-optional\
       --follow-imports=skip \
-      --config-file mypy.ini \
+      --config-file python/mypy.ini \
       --warn-no-return --warn-redundant-casts --warn-unused-ignores\
       $PYFILES || exit 1
 pycodestyle $PYFILES || exit 1
 pydocstyle $PYFILES || exit 1
 flake8 $PYFILES || exit 1
-pylint --rcfile pylintrc $PYFILES || exit 1
+pylint --rcfile python/pylintrc $PYFILES || exit 1
