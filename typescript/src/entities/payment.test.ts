cf50df5c957891d7b7dc39406ab17ba3bb97d69d
Cheng Yu <chengyu0316@gmail.com>
2022-02-22 09:10:08 +1100

    add errors for refund (#509)
    * add errors for refund
    
    * refund function
    
    * write testcase
    
    * fix quote

--- typescript/src/entities/payment.test.ts
+++ typescript/src/entities/payment.test.ts
@@ -1,7 +1,19 @@
+import { setup, mockFetch } from '../test_util';
 import { Merchi } from '../merchi';
+
+setup();
 
 test('can make Payment', () => {
   const merchi = new Merchi();
   const payment = new merchi.Payment();
   expect(payment).toBeTruthy();
+});
+
+test('payment refund', () => {
+  const merchi = new Merchi();
+  const payment = new merchi.Payment();
+  mockFetch(true, { 'amount': 10 }, 200);
+  payment.refund().then(payment => {
+    expect(payment.amount).toEqual(10);
+  });
 });
