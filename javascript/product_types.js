8af8b06c34e5e4aa07c6eccd45bb81324d2b8fbf
Jim Power <jimpower@Jims-MacBook-Pro.local>
2022-03-30 15:34:27 +1100

    updae product types

--- javascript/product_types.js
+++ javascript/product_types.js
@@ -5,6 +5,16 @@ productTypes.add(0, 'supplier MOD (made on demand)');
 productTypes.add(1, 'supplier');
 productTypes.add(2, 'seller');
 productTypes.add(3, 'seller MOD (made on demand)');
+productTypes.add(4, 'chained MOD (made on demand)');
+productTypes.add(5, 'seller chained MOD (made on demand)');
+productTypes.add(6, 'production MOD (made on demand)');
+productTypes.add(7, 'chained supplier');
+productTypes.add(8, 'seller inventory');
+productTypes.add(9, 'supplir inventory');
+productTypes.add(10, 'seller group buy');
+productTypes.add(11, 'supplier resell MOD (made on demand)');
+productTypes.add(12, 'supplier resell');
+productTypes.add(13, 'seller inventory chained');
 
 export const productTypesInts = new Dictionary();
 productTypes.each(function (key, value) {
