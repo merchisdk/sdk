09a331e0af0ce4129d6321f2f275caa28325b6b5
Jim Power <jimpower@Jims-MacBook-Pro.local>
2022-04-04 17:56:48 +1000

    fix syntax

--- python/util/user_types.py
+++ python/util/user_types.py
@@ -4,15 +4,15 @@ CLIENT_DOMAIN = 2  # A user who is a client of one domain
 CLIENT_MULTIPLE_DOMAINS = 3  # A user who is a client of multiple domains
 CLIENT_MERCHI = 4  # A user has a client profile with Merchi
 SELLER_FREE_MERCHI = 5  # A user who has opened a seller store through Merchi
-SELLER_FREE_DOMAIN = 6  # A user who has opened a seller store through a 
-                        # domain/supplier
+SELLER_FREE_DOMAIN = 6  # A user who has opened a seller store through a
+# domain/supplier
 SELLER_PAID = 7  # A user who is paying for a seller store
 SELLER_PAID_MULTIPLE_DOMAINS = 8  # A user who is paying for multiple
-                                  # seller stores
+# seller stores
 SUPPLIER_THIRD_PARTY = 9  # A user who has a "domain supplier" domain
 SUPPLIER_RESTRICTED = 10  # A user who has a restricted supply domain
 SUPPLIER_MERCHI = 11  # A user who has a supply domain which has been
-                      # verified by Merchi
+# verified by Merchi
 UNRESTRICTED = 12  # A user who has a domain which is unrestricted
 
 ALL_TYPES = [
