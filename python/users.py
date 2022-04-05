aad77b49f350e1e3b1cc7b8fd11ffedac47807fa
Amtron <james@newgenstudio.com>
2022-04-06 09:42:05 +1000

    add new user attributes registered_as_guest and registered_under_domain (#527)
    * add new user attributes registered_as_guest and registered_under_domain
    
    * allow registeredUnderDomain to be null
    
    * fix register for doamin type in sdk
    
    Co-authored-by: Jim Power <jimpower@Jims-MacBook-Pro.local>
    Co-authored-by: chengyu <chengyu0316@gmail.com>
    Co-authored-by: Unitix <james@unitix.com.au>

--- python/users.py
+++ python/users.py
@@ -63,6 +63,8 @@ class User(sdk.python.entities.Entity):
     enable_store_notifications = Property(bool)
     is_super_user = Property(bool)
     is_admin_of_subscribed_company = Property(bool)
+    registered_as_guest = Property(bool)
+    registered_under_domain = Property('sdk.python.domains.Domain')
     system_roles = Property(SystemRole)
     user_companies = Property(UserCompany, backref="user")
     companies_need_stripe = Property('sdk.python.companies.Company')
--- typescript/src/entities/user.ts
+++ typescript/src/entities/user.ts
@@ -31,7 +31,7 @@ import { Role,
   BUSINESS_ACCOUNTS,
   ROLES_RANK
 } from '../constants/roles';
-import { UserType } from '../constants/user_types'; 
+import { UserType } from '../constants/user_types';
 import { SystemRoles as SR } from '../constants/system_roles';
 
 import { some } from 'lodash';
@@ -52,6 +52,12 @@ export class User extends Entity {
 
   @User.property()
   public userType?: UserType;
+
+  @User.property()
+  public registeredAsGuest?: boolean;
+
+  @User.property({type: 'Domain'})
+  public registeredUnderDomain?: Domain | null;
 
   @User.property({type: String})
   public password?: string | null;
