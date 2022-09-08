0ecf679373e5156d0075067e70e6f9281e096b5f
chengyu <chengyu0316@gmail.com>
2022-09-09 09:49:57 +1000

    another try

--- python/lint.sh
+++ python/lint.sh
@@ -10,4 +10,4 @@ mypy --ignore-missing-imports --check-untyped-defs --strict-optional\
 pycodestyle $PYFILES || exit 1
 pydocstyle $PYFILES || exit 1
 flake8 $PYFILES || exit 1
-#pylint --rcfile python/pylintrc $PYFILES || exit 1
+pylint --rcfile python/pylintrc $PYFILES || exit 1
