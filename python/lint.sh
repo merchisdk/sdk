2c12d45be040693d6f1ded93e881a0facc6ba9da
chengyu <chengyu0316@gmail.com>
2022-09-09 09:32:13 +1000

    another try

--- python/lint.sh
+++ python/lint.sh
@@ -2,7 +2,7 @@
 
 PYFILES=`find python/ -type f -name "*.py" -not -path "*/tests/*"`
 mypy --ignore-missing-imports --check-untyped-defs --strict-optional\
-      --follow-imports=skip --non-interactive --install-types --no-incremental\
+      --follow-imports=skip --non-interactive --install-types \
       --cache-dir mypy_cache\
       --config-file python/mypy.ini \
       --warn-no-return --warn-redundant-casts --warn-unused-ignores\
