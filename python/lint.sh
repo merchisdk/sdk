8df3dea46a975d9a1ed9ee5cd0eece2f916fc394
chengyu <chengyu0316@gmail.com>
2022-09-09 10:01:09 +1000

    another try

--- python/lint.sh
+++ python/lint.sh
@@ -7,7 +7,7 @@ mypy --ignore-missing-imports --check-untyped-defs --strict-optional\
       --config-file python/mypy.ini \
       --warn-no-return --warn-redundant-casts --warn-unused-ignores\
       $PYFILES || exit 1
-#pycodestyle $PYFILES || exit 1
+pycodestyle $PYFILES || exit 1
 pydocstyle $PYFILES || exit 1
 #flake8 $PYFILES || exit 1
-#pylint --rcfile python/pylintrc $PYFILES || exit 1
+pylint --rcfile python/pylintrc $PYFILES || exit 1
