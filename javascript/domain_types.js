397e17ec132cdac42fff458b3c970bca9869d0a3
Cheng Yu <chengyu0316@gmail.com>
2022-10-05 10:49:11 +1100

    add shopify domain types (#597)

--- javascript/domain_types.js
+++ javascript/domain_types.js
@@ -7,6 +7,9 @@ domainTypes.add(2, 'Seller plus');
 domainTypes.add(3, 'Supplier');
 domainTypes.add(4, 'Restricted supplier');
 domainTypes.add(5, 'Domain supplier');
+domainTypes.add(6, 'Domain client catalogue');
+domainTypes.add(7, 'Domain shopify seller');
+domainTypes.add(8, 'Unrestricted shopify app');
 
 export const domainTypesInts = new Dictionary();
 domainTypes.each(function (key, value) {
