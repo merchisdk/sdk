8c6ae2550b168754a71b95d9e5645708a296c5d0
chengyu <chengyu0316@gmail.com>
2022-11-17 11:59:06 +1100

    fix name

--- python/util/white_label_accessibilities.py
+++ python/util/white_label_accessibilities.py
@@ -1,3 +1,3 @@
 NOT_REACHABLE = 0  # User has wrong DNS configuration
 NO_CERITICATE = 1  # Site can be reached but no valid SSL certificate
-INSTALLING_CERTIFICATE = 2  # Site can be reached with SSL setup
+FULLY_REACHABLE = 2  # Site can be reached with SSL setup
