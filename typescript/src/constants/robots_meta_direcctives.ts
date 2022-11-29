68fde68bc55126598d6631e0df9fe30743e85d62
Jim Power <jimpower@Jims-MacBook-Pro.local>
2022-11-29 19:26:08 +1100

    fix syntax

--- typescript/src/constants/robots_meta_direcctives.ts
+++ typescript/src/constants/robots_meta_direcctives.ts
@@ -1,11 +1,11 @@
 export const ROBOTS_META_DIRECTIVES = [
-  "noindex",
-  "index",
-  "follow",
-  "nofollow",
-  "noimageindex",
-  "none",
-  "noarchive",
-  "nocache",
-  "nosnippet",
-]
+  'noindex',
+  'index',
+  'follow',
+  'nofollow',
+  'noimageindex',
+  'none',
+  'noarchive',
+  'nocache',
+  'nosnippet',
+];
