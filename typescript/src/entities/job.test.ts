a5cd071f463e33f8ff1c74f84480ab02f7ea93a0
Your Name <you@example.com>
2023-01-24 13:22:36 +1100

    adjust testcase

--- typescript/src/entities/job.test.ts
+++ typescript/src/entities/job.test.ts
@@ -72,20 +72,13 @@ test('test deduct job inventory', () => {
   matchingInventoryOne.status = InventoryStatus.CAN_DEDUCT;
   matchingInventoryOne.deductionDate = null;
   job.matchingInventories = [matchingInventoryOne];
-  const correct = [
-    ['id', '1'],
-    ['matchingInventories-0-deductionDate', 'null'],
-    ['matchingInventories-0-inventory-0-id', '1'],
-    ['matchingInventories-0-inventory-count', '1'],
-    ['matchingInventories-0-status', String(InventoryStatus.CAN_DEDUCT)],
-    ['matchingInventories-count', '1'],
-  ];
+  const correct = '{"inventories":[1]}'
   const fetch = mockFetch(true, {}, 200);
   job.deduct([matchingInventoryOne]);
-  expect(Array.from(fetch.mock.calls[0][1]['body'].entries())).toEqual(correct);
+  expect(fetch.mock.calls[0][1]['body']).toEqual(correct);
 
   job.bulkDeduct();
-  expect(Array.from(fetch.mock.calls[1][1]['body'].entries())).toEqual(correct);
+  expect(fetch.mock.calls[1][1]['body']).toEqual(correct);
 });
 
 test('test bulk deduct job without inventories will cause error', () => {
