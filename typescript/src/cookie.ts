51c4bcbcf5efa13b405f5922532ac6ba429fc5ea
s3341458 <you@example.com>
2023-03-31 11:14:53 +1100

    ignore the line to judge this is node js enviroment for test case

--- typescript/src/cookie.ts
+++ typescript/src/cookie.ts
@@ -1,15 +1,15 @@
 export function getCookie(name: string) {
   // code might be executed during SSR
-  if (!document) {
-    return undefined;
-  }
-  const searchPrefix = name + '=',
-    cookies = document.cookie.split(';');
-  for (let i = 0; i < cookies.length; ++i) {
-    let cookie = cookies[i];
-    cookie = cookie.replace(/^\s*/, '');
-    if (cookie.indexOf(searchPrefix) === 0) {
-      return cookie.substring(searchPrefix.length, cookie.length);
+  // istanbul ignore next
+  if (document) {
+    const searchPrefix = name + '=',
+      cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; ++i) {
+      let cookie = cookies[i];
+      cookie = cookie.replace(/^\s*/, '');
+      if (cookie.indexOf(searchPrefix) === 0) {
+        return cookie.substring(searchPrefix.length, cookie.length);
+      }
     }
   }
   return undefined;
