4aa301ab7e35677e0f84bc47af586bb7a7c7ecdf
Jim Power <jimpower@Jims-MacBook-Pro.local>
2024-06-12 10:52:09 +1000

    move attrs

--- python/discount_groups.py
+++ python/discount_groups.py
@@ -14,7 +14,6 @@ class DiscountGroup(sdk.python.entities.Entity):
     date_start = Property(datetime.datetime)
     discount_type = Property(int)
     name = Property(str)
-    code = Property(str)
     discounts = Property("Discount")
     domain = Property("Domain")
     product = Property("Product")
