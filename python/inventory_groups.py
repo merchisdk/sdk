2741b75cd51335f12b3ad916614e54d552ddc00b
Jim Power <jimpower@Jims-MacBook-Pro.local>
2024-09-17 11:07:49 +1000

    add inventory group

---
+++ python/inventory_groups.py
@@ -0,0 +1,29 @@
+import datetime
+import sdk.python.entities
+import sdk.python.users
+from sdk.python.util.business_default import PAYMENT_TYPES
+from sdk.python.entities import Property
+
+
+class InventoryGroup(sdk.python.entities.Entity):
+
+    resource = '/inventory_groups/'
+    json_name = 'inventoryGroup'
+
+    archived = Property(datetime.datetime)
+    id = Property(int)
+    name = Property(str)
+    inventories = Property("sdk.python.invoices.Inventories")
+    products = Property("sdk.python.products.Products")
+    jobs = Property("sdk.python.jobs.Jobs")
+    variation_fields = Property(
+        "sdk.python.variation_fields.VariationFields")
+
+
+class InventoryGroups(sdk.python.entities.Resource):
+
+    entity_class = InventoryGroup
+    json_name = 'inventoryGroups'
+
+
+inventory_groups = InventoryGroups()
