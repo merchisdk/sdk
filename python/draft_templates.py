56a02fffebb65d9342315a9c37494c4097c1205a
Cheng Yu <chengyu0316@gmail.com>
2025-03-27 09:33:21 +1100

    add canvas support attributes to draft templates

--- python/draft_templates.py
+++ python/draft_templates.py
@@ -4,6 +4,7 @@ from sdk.python.entities import Property
 from sdk.python.files import File
 from sdk.python.jobs import Job
 from sdk.python.products import Product
+from sdk.python.variations import VariationField, VariationFieldOption
 
 
 class DraftTemplate(sdk.python.entities.Entity):
@@ -20,6 +21,9 @@ class DraftTemplate(sdk.python.entities.Entity):
     date = Property(datetime.datetime)
     height = Property(int)
     width = Property(int)
+    edited_by_variation_fields = Property(VariationField)
+    selected_by_variation_field_options = Property(VariationFieldOption)
+    original = Property('sdk.python.draft_templates.DraftTemplate')
 
 
 class DraftTemplates(sdk.python.entities.Resource):
